7a11762ce39e08f7086ec72c8084eaf9b7b32e61
CK. <2655257336@qq.com>
2026-08-06 20:24:18 +0800

    feat: 更新构建脚本和配置，优化项目结构

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,5 +3,8 @@ import vueDevTools from "vite-plugin-vue-devtools";
 import { defineConfig, lazyPlugins } from "vite-plus";
 
 export default defineConfig({
+  server: {
+    open: true,
+  },
   plugins: lazyPlugins(() => [vue(), vueDevTools()]),
 });
