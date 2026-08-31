d5616be62f43472e1f3887cf0d0a77d835a17560
CK. <2655257336@qq.com>
2026-08-31 21:40:24 +0800

    feat: 在 UpdateUserRolesDto 中添加 ArrayUnique 装饰器以确保 roleIds 唯一性

--- apps/server/src/modules/iam/users/dto/update-user-roles.dto.ts
+++ apps/server/src/modules/iam/users/dto/update-user-roles.dto.ts
@@ -1,7 +1,8 @@
-import { IsArray, IsInt } from "class-validator";
+import { ArrayUnique, IsArray, IsInt } from "class-validator";
 
 export class UpdateUserRolesDto {
   @IsArray()
+  @ArrayUnique()
   @IsInt({ each: true })
   roleIds!: number[];
 }
